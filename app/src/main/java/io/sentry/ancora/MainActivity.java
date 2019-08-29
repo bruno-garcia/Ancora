f7991206f28929c0ae8121e5811e65fdc3a96379
Bruno Garcia <bruno@brunogarcia.com>
2019-08-29 10:52:45 -0400

    ANR

--- app/src/main/java/io/sentry/ancora/MainActivity.java
+++ app/src/main/java/io/sentry/ancora/MainActivity.java
@@ -13,12 +13,16 @@ import android.view.MenuItem;
 
 import io.sentry.Sentry;
 import io.sentry.android.AndroidSentryClientFactory;
+import io.sentry.event.EventBuilder;
+import io.sentry.event.interfaces.ExceptionInterface;
+import io.sentry.event.interfaces.ExceptionMechanism;
+import io.sentry.event.interfaces.ExceptionMechanismThrowable;
 
 public class MainActivity extends AppCompatActivity {
 
-    static {
-        System.loadLibrary("greetings");
-    }
+//    static {
+//        System.loadLibrary("greetings");
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,12 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Context ctx = this.getApplicationContext();
-        String sentryDsn = "https://46fee3fb0e2a45cca85f2f2c41efe52c@sentry.io/1379099";
+//        String sentryDsn = "https://46fee3fb0e2a45cca85f2f2c41efe52c@sentry.io/1379099";
+        String sentryDsn = "https://46fee3fb0e2a45cca85f2f2c41efe52c@sentry.io/1379099?" +
+                "stacktrace.app.packages=io.sentry.ancora&" +
+                "anr.enable=true&" +
+                "anr.timeoutIntervalMs=1000";
+
         Sentry.init(sentryDsn, new AndroidSentryClientFactory(ctx));
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -50,6 +59,19 @@ public class MainActivity extends AppCompatActivity {
                     unsafeMethod();
                 } catch (Exception e) {
                     Sentry.capture(e);
+                }
+            }
+        });
+
+        FloatingActionButton bla = findViewById(R.id.bla);
+        bla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Try cause ANR (expect default 1 sec for debugging)
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    return;
                 }
             }
         });
